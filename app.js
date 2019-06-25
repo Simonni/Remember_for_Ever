12e95ae133164fb5345ced906a1eb3227b827023
Simonni <szkiflay@gmal.com>
2019-06-25 14:17:20 -0400

    changed the UserPage & added JS function

--- app.js
+++ app.js
@@ -5,3 +5,16 @@
 let redirect = function(){
   document.location.href="./forms/page3.html"
 }
+
+
+
+var form = document.getElementsByClassName('form-signin');
+form.onsubmit = function(e){
+  e.preventDefault();
+  var result = document.getElementById('result');
+  
+result.innerHTML= 'Welcome' + form.inputEmail.value;
+console.log(result)
+  this.reset();
+}
+
